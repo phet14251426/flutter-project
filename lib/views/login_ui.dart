edda869d56b4faca44fec1ba287d10fc1b92c186
Narupon Musiokanon <gizmoment@gmail.com>
2022-10-11 20:24:54 +0700

    New_Login

---
+++ lib/views/login_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginUI extends StatefulWidget {
+  const LoginUI({super.key});
+
+  @override
+  State<LoginUI> createState() => _LoginUIState();
+}
+
+class _LoginUIState extends State<LoginUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
