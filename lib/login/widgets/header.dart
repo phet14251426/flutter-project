f8c2edb70d2ea2f8bb177c644dd40f1d229b1a6a
PONGPALIN PATIPANPIPAT <phet-142514261@hotmail.com>
2022-10-16 19:11:06 +0700

    Login_pagge

---
+++ lib/login/widgets/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:francies_mobie_1/constants/assets.dart';
+ 
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 50,bottom: 50),
+      height: 350,
+      child: Image.asset(Asset.LOGO_IMAGE),
+    );
+  }
+}
